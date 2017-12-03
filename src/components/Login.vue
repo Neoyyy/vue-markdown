e6b626e65c50667977bacc38c9dfb0d830cba9d3
neoyyyy <neoyyyy@neoyyyydeMacBook-Pro.local>
2017-12-03 21:16:39 +0800

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,7 +8,7 @@
   <div v-if="firstChoice=='注册'" class="form-group" id="registname">
     <label for="inputEmail3" class="col-sm-2 control-label">name</label>
     <div class="col-sm-10">
-      <input type="text" class="form-control" id="username" placeholder="nick name">
+      <input type="text" v-model="formdata.nickname" class="form-control" id="username" placeholder="nick name">
     </div>
   </div>
 
@@ -16,20 +16,20 @@
   <div class="form-group" id="email">
     <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+      <input type="email" v-model="formdata.email" class="form-control" id="inputEmail3" placeholder="Email">
     </div>
   </div>
 
    <div class="form-group" id="password">
     <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+      <input type="password" v-model="formdata.password" class="form-control" id="inputPassword3" placeholder="Password">
     </div>
   </div>
 
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-default">Sign in</button>
+      <button type="submit" class="btn btn-default" v-on:click="submit">{{ buttontag }}</button>
     </div>
   </div>
 
@@ -44,25 +44,64 @@ export default {
     return {
       firstChoice : '登录',
       alternative : '注册',
+      buttontag : '登录',
+      formdata : {
+        password : '',
+        nickname : '',
+        email : ''
+      }
 
     }
   },
   methods: {
     toggle: function () {
 
+      //alert(this.firstChoice);
       console.log('点击了');
-      if (firstChoice == '登录'){
-        firstChoice = '注册';
-        alternative = '登录';
-        console.log('切换了');
+      if (this.firstChoice == '登录'){
+        this.firstChoice = '注册';
+        this.alternative = '登录';
+        this.buttontag = '注册';
+        //console.log('切换了');
 
 
       }else{
-        firstChoice = '登录';
-        alternative = '注册';
-        console.log('切换了');
+        this.firstChoice = '登录';
+        this.alternative = '注册';
+        this.buttontag = '登录';
+
+        //console.log('切换了');
 
       }
+
+
+
+
+    },
+    submit: function () {
+      var url = 'http://localhost:3000';
+      //alert(url);
+      console.log('the host :'+this.HOST);
+      var data = {
+        email : this.formdata.email,
+        password : this.formdata.password
+
+      };
+
+      if (this.buttontag == '登录'){
+        url += '/login';
+        console.log('the url:'+url);
+      }else{
+        data.nickname = this.formdata.nickname;
+        url += '/register';
+
+      }
+
+
+      $.post(url,data,function (res) {
+        alert(res.message);
+      })
+
     }
   }
 }
